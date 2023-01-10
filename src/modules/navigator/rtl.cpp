b703b39a932399430fe297db8f6360a9f8564f50
Konrad <konrad@auterion.com>
2023-01-10 20:48:42 +0100

    implement rtl mission subclasses

--- src/modules/navigator/rtl.cpp
+++ src/modules/navigator/rtl.cpp
@@ -50,39 +50,115 @@ using namespace time_literals;
 RTL::RTL(Navigator *navigator) :
 	NavigatorMode(navigator),
 	ModuleParams(navigator),
-	_rtl_direct(navigator)
+	_rtl_direct(navigator),
+	_rtl_mission(navigator),
+	_rtl_mission_reverse(navigator)
 {
-
+	initMission();
 }
 
 void RTL::on_inactivation()
 {
-
+	switch(_rtl_type)
+	{
+	case RtlType::RTL_MISSION_FAST:
+		_rtl_mission.on_inactivation();
+		break;
+	case RtlType::RTL_MISSION_FAST_REVERSE:
+		_rtl_mission_reverse.on_inactivation();
+		break;
+	case RtlType::RTL_DIRECT:
+		_rtl_direct.on_inactivation();
+	default:
+		break;
+	}
 }
 
 void RTL::on_inactive()
 {
+	PlannedMissionInterface::update();
+
 	setRtlType();
+
+	_rtl_mission.on_inactive();
+	_rtl_mission_reverse.on_inactive();
+	_rtl_direct.on_inactive();
 
 	// Limit inactive calculation to 1Hz
     	hrt_abstime now{hrt_absolute_time()};
 	if ((now - _destination_check_time) > 1_s) {
 		_destination_check_time = now;
+	    	const vehicle_global_position_s &global_position = *_navigator->get_global_position();
 
-		const vehicle_global_position_s &global_position = *_navigator->get_global_position();
+	    	const bool global_position_recently_updated = global_position.timestamp > 0
+		&& hrt_elapsed_time(&global_position.timestamp) < 10_s;
 
-		const bool global_position_recently_updated = global_position.timestamp > 0
-				&& hrt_elapsed_time(&global_position.timestamp) < 10_s;
+	    	rtl_time_estimate_s estimated_time{};
+	    	estimated_time.valid = false;
 
-		rtl_time_estimate_s estimated_time{};
-		estimated_time.valid = false;
-
-		// Calculate RTL destination and time estimate only when there is a valid home and global position
-		if (_navigator->home_global_position_valid() && global_position_recently_updated) {
-		    estimated_time = _rtl_direct.calc_rtl_time_estimate();
-		}
+	    	// Calculate RTL destination and time estimate only when there is a valid home and global position
+	    	if (_navigator->home_global_position_valid() && global_position_recently_updated) {
+		    switch(_rtl_type)
+		    {
+			case RtlType::RTL_DIRECT:
+			    estimated_time = _rtl_direct.calc_rtl_time_estimate();
+			    break;
+			case RtlType::RTL_MISSION_FAST:
+			    estimated_time = _rtl_mission.calc_rtl_time_estimate();
+			    break;
+			case RtlType::RTL_MISSION_FAST_REVERSE:
+			    estimated_time = _rtl_mission_reverse.calc_rtl_time_estimate();
+			    break;
+			default:
+			    break;
+		    }
+	    	}
 
 		_rtl_time_estimate_pub.publish(estimated_time);
+	}
+}
+
+void RTL::on_activation()
+{
+	switch(_rtl_type)
+	{
+	case RtlType::RTL_MISSION_FAST:
+		_rtl_mission.on_activation();
+		break;
+	case RtlType::RTL_MISSION_FAST_REVERSE:
+		_rtl_mission_reverse.on_activation();
+		break;
+	case RtlType::RTL_DIRECT:
+		_rtl_direct.on_activation(_enforce_rtl_alt);
+		break;
+	default:
+		break;
+	}
+}
+
+void RTL::on_active()
+{
+	PlannedMissionInterface::update();
+
+	switch(_rtl_type)
+	{
+	case RtlType::RTL_MISSION_FAST:
+		_rtl_mission.on_active();
+		_rtl_mission_reverse.on_inactive();
+		_rtl_direct.on_inactive();
+		break;
+	case RtlType::RTL_MISSION_FAST_REVERSE:
+		_rtl_mission_reverse.on_active();
+		_rtl_mission.on_inactive();
+		_rtl_direct.on_inactive();
+		break;
+	case RtlType::RTL_DIRECT:
+		_rtl_direct.on_active();
+		_rtl_mission_reverse.on_inactive();
+		_rtl_mission.on_inactive();
+		break;
+	default:
+		break;
 	}
 }
 
@@ -91,24 +167,14 @@ void RTL::setRtlType()
 
 	if (_param_rtl_type.get() == 2) {
 		if (hasMissionLandStart()) {
-			_rtl_type = RTL_MISSION_FAST;
+			_rtl_type = RtlType::RTL_MISSION_FAST;
 
 		} else {
-			_rtl_type = RTL_MISSION_FAST_REVERSE;
+			_rtl_type = RtlType::RTL_MISSION_FAST_REVERSE;
 		}
 
 	} else {
-		_rtl_type = RTL_DIRECT;
+		_rtl_type = RtlType::RTL_DIRECT;
 	}
-}
-
-void RTL::on_activation()
-{
-	_rtl_direct.initialize(_enforce_rtl_alt);
-}
-
-void RTL::on_active()
-{
-	_rtl_direct.update();
 }
 
